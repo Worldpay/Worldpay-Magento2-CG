07dfa813ea9d11f97b914e50c78f5abc89507677
Elavarasan Natarajan <elavarasan.natarajan@publicissapient.com>
2020-06-15 22:25:08 +0530

    same site cokie added

--- Model/Request.php
+++ Model/Request.php
@@ -75,7 +75,7 @@ class Request
         // Cookie Set to 2nd 3DS request only.
         $cookie = $this->helper->getWorldpayAuthCookie();
         if ($this->helper->IsThreeDSRequest() && $cookie != "") { // Check is 3DS request
-            $cookie = $cookie.';cookieSameSite="None"';
+            $cookie = $cookie.';SameSite="None"';
             $request->setOption(CURLOPT_COOKIE, $cookie);
         }
 	if ($this->helper->isDynamic3DS2Enabled() && $cookie != "") { // Check is 3DS2 request
