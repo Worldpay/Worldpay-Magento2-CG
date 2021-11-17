4ccb1f7fe42e14b8ac3e4c2396eaab14e77c7766
Chandan Sinha <chasinha@publicisgroupe.net>
2021-11-17 16:37:33 +0000

    SEPA-preferredPaymentMethod issue fix

--- Model/Authorisation/RedirectService.php
+++ Model/Authorisation/RedirectService.php
@@ -105,10 +105,15 @@ class RedirectService extends \Magento\Framework\DataObject
 
     private function _buildRedirectUrl($redirect, $paymentType, $countryCode, $languageCode)
     {
-        $redirect .= '&preferredPaymentMethod=' . $paymentType;
+		if($paymentType == "SEPA_DIRECT_DEBIT-SSL") {
+            //$redirect .= '&preferredPaymentMethod=' . $paymentType;
         $redirect .= '&country=' . $countryCode;
         $redirect .= '&language=' . $languageCode;
-
+		}else{
+	    $redirect .= '&preferredPaymentMethod=' . $paymentType;
+        $redirect .= '&country=' . $countryCode;
+        $redirect .= '&language=' . $languageCode;
+		}
         return $redirect;
     }
 
