98fb30b23ddd817df6e12e7f3ce74d72956f7a6d
¨madalina.ionescu¨ <madalinageorgiana.ionescu@worldpay.com>
2024-10-24 13:03:03 +0300

    move jwt generation in php

--- Controller/ThreeDSecure/Auth.php
+++ Controller/ThreeDSecure/Auth.php
@@ -102,7 +102,7 @@ class Auth extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $threeDSecureChallengeParams = $this->checkoutSession->get3Ds2Params();
-        
+
         $threeDSecureChallengeConfig = $this->checkoutSession->get3DS2Config();
         $directOrderParams = $this->checkoutSession->getDirectOrderParams();
         $orderId = $this->checkoutSession->getAuthOrderId();
@@ -115,7 +115,7 @@ class Auth extends \Magento\Framework\App\Action\Action
         if ($skipSameSiteForIOs) {
             $this->wplogger->info("Inside skip same site block");
             if (isset($cookieValue)) {
-                
+
                 $phpsessId = $cookieValue;
                 $domain = $mhost;
                 $expires = time() + 3600;
@@ -167,9 +167,9 @@ class Auth extends \Magento\Framework\App\Action\Action
             }
         }
         //setcookie("PHPSESSID", $phpsessId, time() + 3600, "/; SameSite=None; Secure;");
-        
+
         if (!$threeDSecureChallengeConfig == null) {
-        
+
             if ($threeDSecureChallengeConfig['challengeWindowType'] == 'iframe') {
                 $iframe = true;
             }
@@ -179,18 +179,18 @@ class Auth extends \Magento\Framework\App\Action\Action
 //            $phpsessId = $_COOKIE['PHPSESSID'];
 //          setcookie("PHPSESSID", $phpsessId, time() + 3600, "/; SameSite=None; Secure;");
             $responseUrl = $this->_url->getUrl('worldpay/threedsecure/authresponse', ['_secure' => true]);
-            
+
             $resContent = '
                 <form name="theForm" id="form" method="POST" action=' . $redirectData->getUrl() . '>
                     <input type="hidden" name="PaReq" value=' . $redirectData->getPaRequest() . ' />
                     <input type="hidden" name="TermUrl" value=' . $responseUrl . ' />
                 </form>';
-            
+
             $resContent .='
                 <script language="Javascript">
                     document.getElementById("form").submit();
                 </script>';
-        
+
             $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
             $result->setHeader('Content-Type', 'text/html');
             $result->setContents($resContent);
@@ -199,9 +199,9 @@ class Auth extends \Magento\Framework\App\Action\Action
             if ($iframe) {
                 $challengeUrl = $this->_url->getUrl("worldpay/hostedpaymentpage/challenge");
                 $imageurl = $this->_assetRepo->getUrl("Sapient_Worldpay::images/cc/worldpay_logo.png");
-                
+
                 $resContent = '
-                    <div id="challenge_window">                        
+                    <div id="challenge_window">
                         <div class="image-content" style="text-align: center;">
                             <img src=' . $imageurl . ' alt="WorldPay"/>
                         </div>
@@ -221,84 +221,37 @@ class Auth extends \Magento\Framework\App\Action\Action
                 return $result;
             } else {
                 $authUrl = $this->_url->getUrl('worldpay/threedsecure/ChallengeAuthResponse', ['_secure' => true]);
-                
-                $resContent =' 
+
+                $payload = [
+                    'ACSUrl' => $threeDSecureChallengeParams['acsURL'],
+                    'Payload' => $threeDSecureChallengeParams['payload'],
+                    'TransactionId' => $threeDSecureChallengeParams['transactionId3DS'],
+                ];
+                $encodedJTW = $this->worldpayHelper->createSecondJWTtoken($authUrl, $payload);
+                $resContent ='
                     <form name= "challengeForm" id="challengeForm"
                     method= "POST"
                     action="' . $threeDSecureChallengeConfig["challengeurl"] . '" >
-                    <!-- Use the above Challenge URL for test, 
+                    <!-- Use the above Challenge URL for test,
                     we will provide a static Challenge URL for production once you go live -->
                         <input type = "hidden" name= "JWT" id= "second_jwt" value= "" />
                         <!-- Encoding of the JWT above with the secret "worldpaysecret". -->
                         <input type="hidden" name="MD" value=' . $orderId . ' />
                         <input type="hidden" name="url" value=' . $authUrl . ' />
-                        <!-- 
-                        Extra field for you to pass data in to the challenge that will be included in the post 
-                        back to the return URL after challenge complete 
+                        <!--
+                        Extra field for you to pass data in to the challenge that will be included in the post
+                        back to the return URL after challenge complete
                         -->
                     </form>';
-                
+
                     $resContent .='
-                    <script src="//cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/hmac-sha256.js"></script>
-                    <script src="//cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/enc-base64-min.js">
-                    </script>
                     <script language="Javascript">
-                        var header = {
-                            "typ": "JWT",
-                            "alg": "HS256"
-                        };            
-                        var iat = Math.floor(new Date().getTime()/1000);
-                        var jti = uuidv4();
-                        var data = {
-                            "jti": jti,
-                            "iat": iat,
-                            "iss": "' . $threeDSecureChallengeConfig["jwtIssuer"] . '",
-                            "OrgUnitId": "' . $threeDSecureChallengeConfig["organisationalUnitId"] . '",
-                            "ReturnUrl": "' . $authUrl . '",
-                            "Payload": {
-                                "ACSUrl": "' . $threeDSecureChallengeParams['acsURL'] . '",
-                                "Payload": "' . $threeDSecureChallengeParams['payload'] . '",
-                                "TransactionId": "' . $threeDSecureChallengeParams['transactionId3DS'] . '"
-                                },
-                            "ObjectifyPayload": true
-                        };
-                        var secret = "' . $threeDSecureChallengeConfig["jwtApiKey"] . '";
-
-                        var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
-                        var encodedHeader = base64url(stringifiedHeader);
-
-                        var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
-                        var encodedData = base64url(stringifiedData);
-                        var signature = encodedHeader + "." + encodedData;
-                        signature = CryptoJS.HmacSHA256(signature, secret);
-                        signature = base64url(signature);
-                        var encodedJWT = encodedHeader + "." + encodedData + "." + signature;
-                        document.getElementById("second_jwt").value = encodedJWT;
-                        function uuidv4() {
-                            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function(c){
-                                var crypto = window.crypto || window.msCrypto;
-                                return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-                            });
-                        }
-
-                        function base64url(source) {
-                            // Encode in classical base64
-                            var encodedSource = CryptoJS.enc.Base64.stringify(source);
-
-                            // Remove padding equal characters
-                            encodedSource = encodedSource.replace(/=+$/, "");
-
-                            // Replace characters according to base64url specifications
-                            encodedSource = encodedSource.replace(/\+/g, "-");
-                            encodedSource = encodedSource.replace(/\//g, "_");
-
-                            return encodedSource;
-                        }
+                        document.getElementById("second_jwt").value = "' . $encodedJTW . '";
                         window.onload = function()
                         {
                           // Auto submit form on page load
                           document.getElementById("challengeForm").submit();
-                        } 
+                        }
                     </script>';
 
                 $this->checkoutSession->uns3DS2Params();
